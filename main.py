ae501ef16a31a84e2d4531405e32672502ea070c
maria <maria.vyukhina@mail.ru>
2023-04-18 17:06:31 +0400

    import and call function subtract_numbers in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from funcs import sums_up_numbers
+from funcs import sums_up_numbers, subtract_numbers
 
 result = sums_up_numbers(3, 6)
+result_2 = subtract_numbers(7, 2)
 
-print(result)
+print(result_2)
