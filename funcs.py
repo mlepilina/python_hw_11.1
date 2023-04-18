90c8ee48b0ea881a6865863f1f6c3836cb177fc5
maria <maria.vyukhina@mail.ru>
2023-04-18 16:57:41 +0400

    write subtract_numbers in funcs.py

--- funcs.py
+++ funcs.py
@@ -3,3 +3,8 @@ def sums_up_numbers(number_1, number_2):
     sum_of_numbers = number_1 + number_2
     return sum_of_numbers
 
+
+def subtract_numbers(number_1, number_2):
+    subtraction_of_numbers = number_1 - number_2
+    return subtraction_of_numbers
+
